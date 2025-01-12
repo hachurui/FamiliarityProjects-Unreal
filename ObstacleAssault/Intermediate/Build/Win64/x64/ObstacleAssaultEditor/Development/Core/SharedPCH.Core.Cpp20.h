74cb5ce0768b6967834cd22e29f899c2a6f3f90c
hachurui <loganmiholics@gmail.com>
2025-01-12 18:47:08 -0500

    Beginning of Obstacle Assault
    Grabbed the assets needed for plan. Changed the character body the player controls, changed its mesh. Created a C++ class MovingPlatform and had it built.
    Also added a My Notes folder where all future notes will be found.

---
+++ ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/logan/Desktop/Creative Endeavors/Game Dev/Unreal/Tutorial Games/ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/logan/Desktop/Creative Endeavors/Game Dev/Unreal/Tutorial Games/ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
